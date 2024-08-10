7476901764fcd3037d6c9f1c3db4060195a03769
JDHS1978 <jorgerosajr123@gmail.com>
2024-08-10 14:31:17 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
+'''C
+#include<stdio.h>  
+  
+int main(){
+  printf("Hello,School\n")
+  return 0; 
 }
